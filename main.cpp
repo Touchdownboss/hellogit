4041d7d195b79e8e27b7d33354c727ef9c3e2f5c
Touchdownboss <Touchdownboss@users.noreply.github.com>
2017-09-13 15:35:29 -0400

    chages to main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	vector<Foods> f0 = {{1, "iron"},{2, "fibre"},{3,"carbohytdrates"},{4,"calcium"},{5,"vitamins"}};
 
-	for auto(f::f0)
+	for (auto f:f0)
 	{
 		cout<<f.getPos()<<""<<f.getType()<<endl;
 	}
