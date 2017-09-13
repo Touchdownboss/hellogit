8a74f29bead8b36ee21a9b2f05e837f0c6afda0c
Touchdownboss <Touchdownboss@users.noreply.github.com>
2017-09-13 14:47:07 -0400

    Added client file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include 'FoodClass.h'
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	vector<Foods> f0 = {{1, "iron"},{2, "fibre"},{3,"carbohytdrates"},{4,"calcium"},{5,"vitamins"}};
+
+	for auto(f:f0)
+	{
+		cout<<f.getPos()<<""<<f.getType()<<endl;
+	}
+}
